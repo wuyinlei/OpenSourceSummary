cf3e9a5f3b430f006a0f92bf6f12551742d419ef
wuyinlei <power1118wu@gmail.com>
2017-12-07 17:40:16 +0800

    Initial commit

---
+++ app/src/main/java/ruolan/com/opensourcesummary/TestBean.java
@@ -0,0 +1,16 @@
+package ruolan.com.opensourcesummary;
+
+/**
+ * Created by wuyinlei on 2017/12/7.
+ *
+ * @function 返回的接收bean
+ */
+
+public class TestBean {
+
+    String name;
+    int age;
+    String address;
+    String city;
+
+}
